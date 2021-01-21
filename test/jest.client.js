48b6b6adad590a0f80395f328e9bd25c7634a85a
Taylor Stauss <ttstauss@gmail.com>
2021-01-21 14:26:25 -0700

    chore: add jest-axe for accessibility testing

--- test/jest.client.js
+++ test/jest.client.js
@@ -8,5 +8,6 @@ module.exports = {
   testMatch: ['**/__client_tests__/**/*.js'], // where to look for client test files
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect', // load jest-dom for better assertions into all test files
+    'jest-axe/extend-expect', // load aXe for better accessibility assertions into all test files
   ],
 }
